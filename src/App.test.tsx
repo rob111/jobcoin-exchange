50b2ed2c104b8afaf81be6af9ceb1e6b81927c19
Andrey Knyazev <aknyazev@gannett.com>
2022-02-07 16:39:37 -0500

    setup dashboard

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Sign In With Your Jobcoin Address/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
+import {Dashboard} from './components/Dashboard';
+import {Login} from './components/Login';
+import {useAddress} from './useAddress';
 
-function App() {
+const App = (): JSX.Element => {
+  const {address, setAddress} = useAddress();
+  
+  if (!address) {
+    return <Login setAddress={setAddress} />
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Dashboard address={address} />} />
+        <Route path='/dashboard' element={<Dashboard address={address} />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
